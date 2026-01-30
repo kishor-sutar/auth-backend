dcae3d6910f98c4a3d7c4f62c2107d699dd701d1
kishor sutar <kishorsutar552003@gmail.com>
2026-01-31 02:12:10 +0530

    swagger config is readdy

---
+++ config/swagger.js
@@ -0,0 +1,36 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Auth Backend API",
+            version: "1.0.0",
+            description: "JWT + Refresh Token + Google OAuth Authentication API"
+        },
+        servers: [
+            {
+                url: "http://localhost:5000"
+            }
+        ],
+        components: {
+            securitySchemes: {
+                cookieAuth: {
+                    type: "apiKey",
+                    in: "cookie",
+                    name: "token"
+                }
+            }
+        },
+        security: [
+            {
+                cookieAuth: []
+            }
+        ]
+    },
+    apis: ["./routes/*.js"]
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
