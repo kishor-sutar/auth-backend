be7b034b7f3576b467475fbb394e832acfdf6ea9
Kishor Nandkumar Sutar <kishorsutar552003@gmail.com>
2026-02-05 08:34:04 +0530

    Enhance CORS and Swagger UI config
    Updated CORS origin handling and added custom CSS for Swagger UI.

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const swaggerSpec = require("./config/swagger");
 
 
 const app = express();
-
+connectDB();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" 
+        ? "https://your-frontend-domain.com" // Update this later for your frontend
+        : "http://localhost:5173",
     credentials: true
 }));
 
@@ -23,7 +25,7 @@ app.use(passport.initialize());
 
 app.use(express.json());
 
-connectDB();
+
 
 app.use("/api/auth", authRoutes);
 
@@ -31,7 +33,13 @@ app.get("/", (req, res) => {
     res.send("server is running from AUTH-BACKEND");
 });
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}/`);
